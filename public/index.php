83b0676acf7cb01a38dee648035ca00368d32885
wensun552150 <552150@student.saxion.nl>
2025-03-17 12:57:41 +0100

    Initiate php developing environment
    My first hello world in php!

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+use Slim\Factory\AppFactory;
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name!");
+    return $response;
+});
+
+$app->run();
